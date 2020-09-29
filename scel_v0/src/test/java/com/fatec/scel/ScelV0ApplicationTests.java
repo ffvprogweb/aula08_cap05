1ea36935ab40a7038614a93bf63d5f294180f85c
55119 <55119@192.168.100.6>
2020-09-29 08:32:29 -0300

    Versão inicial

---
+++ scel_v0/src/test/java/com/fatec/scel/ScelV0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.scel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScelV0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
